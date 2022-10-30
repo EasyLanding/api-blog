2fe3f0a09a6acd679d8d2295e869c629f274fb2c
Georgiy Antonevich <goshka.ruu@mail.ru>
2022-10-30 16:40:08 +0300

    update PostController
    Signed-off-by: Georgiy Antonevich <goshka.ruu@mail.ru>

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 import express from 'express'
 import mongoose from 'mongoose'
-import Post from './Post.js'
+import router from './router.js'
 
 const PORT = 5000
 const DB_URL =
   'mongodb+srv://backend-blog-userApi:user@cluster0.grvleb6.mongodb.net/?retryWrites=true&w=majority'
 const app = express()
 app.use(express.json())
-app.post('/', async (req, res) => {
-  try {
-    const { author, title, text } = req.body
-    const post = await Post.create({ author, title, text })
-    console.log(req.body)
-    res.json(post)
-  } catch (e) {
-    req.status(500).json(e)
-  }
-})
+app.use('/api', router)
 
 async function appStart() {
   try {
